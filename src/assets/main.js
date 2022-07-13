9827a76bd53c110bd554b740e22807a8867cb10c
Miguel Velásquez <vmiguel.xy@gmail.com>
2022-07-13 16:26:56 -0400

    Cambio del Tema Principal de la Página.
    La Landing Page ahora es de "Kuroko's Basketball" en vez de "The Seven Deadly Sins" debido a que no pude encontrar openings que poder usar embebido de este último. Y por suerte sí encontré de Kuroko's Basketball.
    Además, se añadió un .css para poder incorporar los iframes de los reproductores de youtube para que se pueda ver los openings desde la web.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const HTML_CONTENT = null || document.getElementById("content");
-const API = "https://youtube-v31.p.rapidapi.com/playlistItems?playlistId=PLSEoGpRPA4UhycGXWKb-4RRdbNLJsYphG&part=snippet&maxResults=50";
+const HTML_CONTENT = document.getElementById("content");
+const API = "https://youtube-v31.p.rapidapi.com/playlistItems?playlistId=PLwrcY2kNSwjB8oPiRnudt1kiJjN4UhEqc&part=snippet&maxResults=50";
 const DEFAULT_OPTIONS = {
 	method: "GET",
 	headers: {
@@ -20,25 +20,51 @@ async function getVideosFromPlaylist() {
 	try {
 		const playlistFullObject = await fetchData(API, DEFAULT_OPTIONS);
 		const videos = playlistFullObject.items;
+
+		const videosAsHtml = videos.map(jsonToHtml).join("");
 		
-		const videosAsHtml = videos.map( video => `
-			<div class="group relative">
-			  <div
-				class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-				<img src="${video.snippet.thumbnails.high.url}" alt="" class="w-full">
+		function jsonToHtml(video, index) {
+			let videoTitle = `Opening ${index+1}: `;
+			switch (index) {
+				case 0:
+					videoTitle += "Can Do (from GRANRODEO)";
+					break;
+				case 1:
+					videoTitle += "RIMFIRE (from GRANRODEO)";
+					break;
+				case 2:
+					videoTitle += "The Other Self (from GRANRODEO)";
+					break;
+				case 3:
+					videoTitle += "Hengen Jizai no Magical Star (from GRANRODEO)";
+					break;
+				case 4:
+					videoTitle += "Punky Funky Love (from GRANRODEO)";
+					break;
+				case 5:
+					videoTitle += "ZERO (from Kenshō Ono)";
+					break;
+				case 6:
+					videoTitle += "Memories (from GRANRODEO)";
+					break;
+			}
+			
+			return `
+			<div class="">
+			  <div class="flex justify-center">
+				<iframe width="426" height="240" src="https://www.youtube.com/embed/${video.snippet.resourceId.videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="youtube-embed-player"></iframe>
 			  </div>
-			  <div class="mt-4 flex justify-between">
+			  <div class="mt-2 flex justify-center text-center">
 				<h3 class="text-sm text-gray-700">
 				  <span aria-hidden="true" class="absolute inset-0"></span>
-				  ${video.snippet.title}
+				  ${videoTitle}
 				</h3>
 			  </div>
-			</div>`
-		).join("");
+			</div>`;
+		}
 		
 		HTML_CONTENT.innerHTML = videosAsHtml;
-		
-		
+
 	} catch (error) {
 		console.error(error);
 		alert("Ocurrió un error 😔.");
